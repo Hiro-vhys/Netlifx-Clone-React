12456170a8a3b5fda7ebe6aa27bc8688e2104dae
Hiro-vhys <victor.h.y.s@hotmail.com>
2021-04-01 12:25:06 -0300

    add png and update README

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default ({black})=>{
     return(
         <header className={black ?"black" : ''}>
             <div className="header--logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"/>
+                <img src="/imgs/Netflix_Logo_RGB.png"/>
             </div>
             <div className="header--user">
                 <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
